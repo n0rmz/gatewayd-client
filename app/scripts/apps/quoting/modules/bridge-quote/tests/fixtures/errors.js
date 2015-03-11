1f81d8a843f4dfcd581a9a2f896c0cc7fc5107b8
Zhenyu Li <zhenyu@ripple.com>
2015-03-11 15:12:18 -0700

    Address comments

--- app/scripts/apps/quoting/modules/bridge-quote/tests/fixtures/errors.js
+++ app/scripts/apps/quoting/modules/bridge-quote/tests/fixtures/errors.js
@@ -1,27 +1,27 @@
 var data = {
-    success: true,
+    success: false,
     bridge_payments: [
         {
-            state: 'quote',
+            state: '',
             created: '',
             source: {
-                uri: 'acct:test1@localhost:5000',
-                address: 'rhY5y4NB7A1TuKgVNrRoSfk41uZasmbDnS',
+                uri: '',
+                address: '',
                 account_number: 1111
             },
             wallet_payment: {
-                destination: 'rphrLBy39abbhUTta1CWtHryYHVcvrTWeX?dt=3',
+                destination: '?',
                 primary_amount: {
                     amount: 1,
                     currency: 'XRP',
                     issuer: ''
                 },
-                destination_account_number: '4123'
+                destination_account_number: 4123
             },
             destination: {
-                uri: 'acct:test2@localhost:5000'
+                uri: ''
             },
-            uuid: '38e48a98-7e6d-4a39-ab49-528da78af959'
+            uuid: ''
         }
     ]
 };
